27849300ca6323da4ad2b031ee52f72c550669d2
Skip <skiphog@mail.ru>
2018-02-23 22:47:39 +0300

    add Article 0.5

--- app/template/articles/one.php
+++ app/template/articles/one.php
@@ -27,24 +27,38 @@ if (!$article = $dbh->query($sql)->fetch()) {
 
 <?php $this->start('style'); ?>
 <style>
-    .articles{color:#444}
-    .article-post{padding:10px;margin-bottom:40px;}
-    .article-header{padding:5px;background-color:#ebebf3;margin-bottom:5px;border-radius:4px}
+    .article-post{color:#444;padding:10px;margin-bottom:40px;}
+    .article-header{padding:5px;background-color:#ebebf3;margin-bottom:5px;border-radius:4px;box-shadow:rgba(0, 0, 0, .09) 0 2px 0;}
     .article-title{font-size:28px;margin-bottom:20px;font-family:inherit}
     .article-title a{color:inherit}
     .article-time{font-style:italic;color:#777;margin:0}
+    .article-form textarea{
+        font-size: 14px;
+        font-family: inherit;
+        width: 100%;
+        height: 80px;
+        display: block;
+        background: #fafafa;
+        border: none;
+        border-radius: 2px;
+        box-shadow: inset 0 1px 1px 0 rgba(0, 0, 0, .1);
+        outline: 0;
+        padding: 8px;
+        margin-bottom: 10px;
+    }
+
 </style>
 <?php $this->stop(); ?>
 
 <?php $this->start('content'); ?>
 <div class="breadcrumbs">
-    <p></p>
+    <a href="/story_<?php echo $article['topic']; ?>_1"><?php echo html($article['topicname']); ?></a>
 </div>
 
 <article class="article-post">
     <header class="article-header">
        <h1 class="article-title"><?php echo html($article['title']); ?></h1>
-        <p class="article-date"><?php echo (new \Swing\Components\SwingDate($article['time']))->format('d-m-Y'); ?></p>
+        <p class="article-time"><?php echo (new \Swing\Components\SwingDate($article['time']))->format('d-m-Y'); ?></p>
     </header>
 
     <main class="article-main">
@@ -53,7 +67,7 @@ if (!$article = $dbh->query($sql)->fetch()) {
     </main>
 </article>
 <?php if($myrow->isUser()) : ?>
-    <form method="post">
+    <form class="article-form" method="post">
         <textarea id="article-message" name="message" placeholder="Ваш комментарий"></textarea>
         <button type="submit">Отправить</button>
     </form>

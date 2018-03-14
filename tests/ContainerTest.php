2d9092589b0dc8aafa8acb3c68e0c2eb3be9168f
Skip <skiphog@mail.ru>
2018-03-14 18:32:48 +0300

    add test for Container

---
+++ tests/ContainerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use System\Container;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ContainerTest
+ */
+class ContainerTest extends TestCase
+{
+
+    public function testGetObject()
+    {
+        $this->assertInstanceOf(StdClass::class, Container::get(StdClass::class));
+    }
+
+    public function testGetCachedObject()
+    {
+        $this->assertEquals(Container::get(StdClass::class), Container::get(StdClass::class));
+    }
+
+    public function testSet()
+    {
+        $class = new StdClass();
+        Container::set('std', $class);
+        $this->assertEquals($class, Container::get('std'));
+        Container::set('std', $class);
+        $this->assertEquals($class, Container::get('std'));
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testNotExistsClassInContainer()
+    {
+        $class = Container::get('NotExists');
+    }
+
+    public function testSetClosure()
+    {
+        Container::set('closure', function () {
+            return new StdClass();
+        });
+
+        $this->assertInstanceOf(StdClass::class, Container::get('closure'));
+    }
+}
